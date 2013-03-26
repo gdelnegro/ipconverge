50228f69c29557c74c04c4d85ab4116b643b8535
gustavo <gustavo@gustavo-Vostro-3550>
2013-03-26 15:48:58 -0300

    Adicionando BD

---
+++ application/models/DbTable/Campanha.php
@@ -0,0 +1,10 @@
+<?php
+
+class Application_Model_DbTable_Campanha extends Zend_Db_Table_Abstract
+{
+
+    protected $_name = 'campanha';
+
+
+}
+
---
+++ application/models/DbTable/Sms.php
@@ -0,0 +1,10 @@
+<?php
+
+class Application_Model_DbTable_Sms extends Zend_Db_Table_Abstract
+{
+
+    protected $_name = 'sms';
+
+
+}
+
