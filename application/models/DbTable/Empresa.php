1772d7c733e6be72320526d7862d7318263966f6
gustavo <gustavo@gustavo-Vostro-3550>
2013-03-26 23:36:46 -0300

    Alteração no Modelo do banco de dados

--- application/models/DbTable/Empresa.php
+++ application/models/DbTable/Empresa.php
@@ -5,7 +5,15 @@ class Application_Model_DbTable_Empresa extends Zend_Db_Table_Abstract
 
     protected $_name = 'empresa';
     protected $_primary = 'id';
+    protected $_dependentTables = array('campanha','empresa','usuario','sms','financeiro');
 
-
+    protected $_referenceMap = array(
+        'Financeiro' => array(
+            'columns' => 'empresa',
+            'refTableClass' =>'Aplication_Model_DbTable_Empresa',
+            'refColumns' => 'id'
+            
+        )
+    );
 }
 
